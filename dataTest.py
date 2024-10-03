034861196c290a6de1efb60bbea8b23ffc861a94
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-10-03 19:17:49 +0900

    generic

--- dataTest.py
+++ dataTest.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import pytest 
-from   expr   import load_data
+from   dirty_code   import load_data
 
 
 
@@ -16,3 +16,16 @@ def test_load_data():
     assert isinstance(df, pd.DataFrame), "Output should be a DataFrame"
     # Optionally, you can also check if the DataFrame is not empty
     assert not df.empty, "DataFrame should not be empty"
+
+
+
+
+
+
+
+'''
+X_train, X_test, y_train, y_test = split_data(df)
+vectorizer, X_train_vec, X_test_vec = preprocess_text(X_train, X_test)
+model = train_model(X_train_vec, y_train)
+result  = evaluate_model(model, X_test_vec, y_test)
+'''
