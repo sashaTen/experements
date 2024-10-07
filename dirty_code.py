8b904e0c9b785691276f94ea6fca18d9ca322123
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-10-07 17:20:40 +0900

    now    i  made  the  autretrain   functions

--- dirty_code.py
+++ dirty_code.py
@@ -1,17 +1,18 @@
 from  expr   import   load_data     ,   zen_sentiment_analysis_pipeline
 ## counts number  of the rows   in  df  and  if  >1000 retrains 
-
-def check_number_samples(df):
+url = 'https://raw.githubusercontent.com/surge-ai/stock-sentiment/main/sentiment.csv'
+def check_number_samples(df ,  url):
     if  df.shape[0]> 490:
         print('the   autoretrain  is  caused ')
+        zen_sentiment_analysis_pipeline(url)
         return    
     else :
         print('not   enough samples  for  retrain ' ,    df.shape[0])
         return 
     
 
-df   =   load_data()
-check_number_samples(df)
+df   =   load_data(url)
+#check_number_samples(df ,  url)
 
 
 
