a430e8c6c1dfc4967b6abcab66fa89a8fa8354ec
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-10-07 16:48:58 +0900

    zen_   is added   to   steps

--- dirty_code.py
+++ dirty_code.py
@@ -1,3 +1,17 @@
+from  expr   import   load_data
+## counts number  of the rows   in  df  and  if  >1000 retrains 
+
+def check_number_samples(df):
+    if  df.shape[0]> 1000:
+        print('more than 1000')
+        return    
+    else :
+        print('not   enough samples  for  retrain ')
+        return 
+    
+
+
+
 
 
 
--- expr.py
+++ expr.py
@@ -55,28 +55,23 @@ def evaluate_model(model, X_test_vec, y_test) :
     return   f1
 
 #
-df = load_data()
-X_train, X_test, y_train, y_test = split_data(df)
-vectorizer, X_train_vec, X_test_vec = preprocess_text(X_train, X_test)
-model = train_model(X_train_vec, y_train)
-result  = evaluate_model(model, X_test_vec, y_test)
-print(result)
 
 
 
 
-'''
+
+
 
 # Step 1: Load data
 @step
-def load_data() -> pd.DataFrame:
+def zen_load_data() -> pd.DataFrame:
     url = 'https://raw.githubusercontent.com/surge-ai/stock-sentiment/main/sentiment.csv'
     df = pd.read_csv(url)
     return df
 
 # Step 2: Split data into train and test sets
 @step
-def split_data(df: pd.DataFrame) -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series]:
+def  zen_split_data(df: pd.DataFrame) -> Tuple[pd.Series, pd.Series, pd.Series, pd.Series]:
     X_train, X_test, y_train, y_test = train_test_split(
         df['Tweet Text'], df['Sentiment'], test_size=0.2, random_state=42
     )
@@ -84,7 +79,7 @@ def split_data(df: pd.DataFrame) -> Tuple[pd.Series, pd.Series, pd.Series, pd.Se
 
 # Step 3: Preprocess and vectorize the text data
 @step
-def preprocess_text(X_train: pd.Series, X_test: pd.Series) -> Tuple[CountVectorizer,csr_matrix, csr_matrix]:
+def  zen_preprocess_text(X_train: pd.Series, X_test: pd.Series) -> Tuple[CountVectorizer,csr_matrix, csr_matrix]:
     vectorizer = CountVectorizer(stop_words='english', max_features=1000)
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
@@ -92,14 +87,14 @@ def preprocess_text(X_train: pd.Series, X_test: pd.Series) -> Tuple[CountVectori
 
 # Step 4: Train the model
 @step
-def train_model(X_train_vec: csr_matrix, y_train: pd.Series) -> LogisticRegression:
+def  zen_train_model(X_train_vec: csr_matrix, y_train: pd.Series) -> LogisticRegression:
     model = LogisticRegression()
     model.fit(X_train_vec, y_train)
     return model
 
 # Step 5: Evaluate the model
 @step
-def evaluate_model(model: LogisticRegression, X_test_vec: csr_matrix, y_test: pd.Series) -> float:
+def  zen_evaluate_model(model: LogisticRegression, X_test_vec: csr_matrix, y_test: pd.Series) -> float:
     y_pred = model.predict(X_test_vec)
     accuracy = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average='weighted')
@@ -108,17 +103,17 @@ def evaluate_model(model: LogisticRegression, X_test_vec: csr_matrix, y_test: pd
 
 # Pipeline to connect all the steps
 @pipeline
-def sentiment_analysis_pipeline():
-    df = load_data()
-    X_train, X_test, y_train, y_test = split_data(df)
-    vectorizer, X_train_vec, X_test_vec = preprocess_text(X_train, X_test)
-    model = train_model(X_train_vec, y_train)
-    evaluate_model(model, X_test_vec, y_test)
+def  zen_sentiment_analysis_pipeline():
+    df =  zen_load_data()
+    X_train, X_test, y_train, y_test =  zen_split_data(df)
+    vectorizer, X_train_vec, X_test_vec =  zen_preprocess_text(X_train, X_test)
+    model =  zen_train_model(X_train_vec, y_train)
+    zen_evaluate_model(model, X_test_vec, y_test)
 
 # Run the pipeline
 if __name__ == "__main__":
-   sentiment_analysis_pipeline()
-   '''
+    zen_sentiment_analysis_pipeline()
+   
 
 
 
