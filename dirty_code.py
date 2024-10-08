0c4c7f5ca2c46a71c796b4d2d67cafdfc8adf59e
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-10-08 21:00:16 +0900

    now the  if   pipeline    retrained  the  latest  model  is   used  for the    inference

--- dirty_code.py
+++ dirty_code.py
@@ -1,4 +1,5 @@
 from  expr   import   load_data     ,   zen_sentiment_analysis_pipeline
+from zenml.client import Client
 ## counts number  of the rows   in  df  and  if  >1000 retrains 
 url = 'https://raw.githubusercontent.com/surge-ai/stock-sentiment/main/sentiment.csv'
 def check_number_samples(df ,  url):
@@ -12,10 +13,28 @@ def check_number_samples(df ,  url):
     
 
 df   =   load_data(url)
-#check_number_samples(df ,  url)
+check_number_samples(df ,  url)
+
+client = Client()
+
+my_runs_on_current_stack = client.list_pipeline_runs(
+    stack_id=client.active_stack_model.id,  # on current stack
+    user_id=client.active_user.id,  # ran by you
+    sort_by="desc:start_time",  # last 10
+    size=10,
+)
 
 
+latest_pipeline_run_id   =    my_runs_on_current_stack[0].id
 
+pipeline_run_id = latest_pipeline_run_id  
+pipeline_run = client.get_pipeline_run(pipeline_run_id)
 
+# Fetch the specific step output artifacts, including model and vectorizer
+zen_train_model_step = pipeline_run.steps.get('zen_train_model')
+model_artifact = zen_train_model_step.outputs['output'].load()
 
-
+zen_preprocess_text_step = pipeline_run.steps.get('zen_preprocess_text')
+vectorizer_artifact = zen_preprocess_text_step.outputs['output_1'].load()
+print(model_artifact)
+print(vectorizer_artifact)
